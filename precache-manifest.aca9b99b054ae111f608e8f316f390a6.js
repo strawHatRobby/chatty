86eee590a213f0ce4a7e58d0c42ffdd9c7e37331
robs100DaysOfCode <robfr93@icloud.com>
2019-01-21 13:24:31 +0530

    Updates

---
+++ precache-manifest.aca9b99b054ae111f608e8f316f390a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "49e5b7af4590a7a6b2380c27ddedc53c",
+    "url": "/chatty/static/media/tenor.49e5b7af.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chatty/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3496d6790826cdebffe",
+    "url": "/chatty/static/js/runtime~main.c3496d67.js"
+  },
+  {
+    "revision": "c5712b6c041b6a222cb9",
+    "url": "/chatty/static/js/main.c5712b6c.chunk.js"
+  },
+  {
+    "revision": "e1a3f7b30016f99c809e",
+    "url": "/chatty/static/js/1.e1a3f7b3.chunk.js"
+  },
+  {
+    "revision": "c5712b6c041b6a222cb9",
+    "url": "/chatty/static/css/main.c056419a.chunk.css"
+  },
+  {
+    "revision": "91e9637d3fa8c8befb2f4fc30710ce58",
+    "url": "/chatty/index.html"
+  }
+];
